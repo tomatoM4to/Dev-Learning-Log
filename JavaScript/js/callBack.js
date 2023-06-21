625cd40c7567508261c37ea0464278edde3ae7ff
tomato <tomatomato2121@gmail.com>
2023-06-21 13:18:46 +0900

    callback 함수 정리, callback hell 예제 작성

--- JavaScript/js/callBack.js
+++ JavaScript/js/callBack.js
@@ -1,5 +1,95 @@
-let callBack = JSON.stringify(true);
-console.log(callBack) // true
+// 1초뒤에 내가 전달한 함수를 다시 불러줘 call back
+setTimeout(() => console.log(`time out`), 1000)
+console.log(1)
 
 
 
+
+
+// call back example
+document.querySelector('.title').addEventListener('click', () => {
+    // call back
+})
+
+
+const first = (a) => {
+    console.log(`first call`);
+    a();
+}
+
+first(() => console.log(`second call`))
+
+
+
+
+
+
+// 동기 콜백
+function printImmediately(print) {
+    print();
+}
+printImmediately(() => console.log(`print immediately.`))
+
+// 비동기 콜백
+
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
+}
+
+printWithDelay(() => console.log(`print with delay.`), 2000);
+
+
+
+// loginUser를 통해 로그인, getRoles를 통해 user의 상태를 받아오는 예제 Backend 코드
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
+        setTimeout(() => {
+            if (
+                (id === 'ellie' && password === 'dream') ||
+                (id === 'coder' && password === 'academy')
+            ) {
+                onSuccess(id);
+            } else {
+                onError();
+            }
+        }, 2000);
+    }
+
+    getRoles(user, onSuccess, onError) {
+        setTimeout(() => {
+            if (user === 'ellie') {
+                onSuccess({ name: 'ellie', role: 'admin' });
+            } else {
+                onError();
+            }
+        }, 1000);
+    }
+}
+
+const userStorage = new UserStorage();
+const submitBtn = document.querySelector('#submitBtn');
+submitBtn.addEventListener('click', () => {
+    const idInput = document.querySelector('#idInput');
+    const passwordInput = document.querySelector('#passwordInput');
+    const id = idInput.value;
+    const password = passwordInput.value;
+    idInput.value = '';
+    passwordInput.value = '';
+
+    userStorage.loginUser(
+        id,
+        password,
+        user => {
+            userStorage.getRoles(
+                user,
+                userWithRole => {
+                    alert(
+                        `Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+                    );
+                },
+                () => alert(`Nomal user`)
+            );
+        },
+        () => alert(`로그인 에러`)
+    );
+});
